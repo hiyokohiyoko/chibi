b8fb36cd39d3fa498c91738ccb58826f76c55c0b
HASEGAWA Kimiko <21716072@jwu.ac.jp>
2019-10-07 16:43:50 +0900

    added gcd

--- rational.py
+++ rational.py
@@ -1,8 +1,10 @@
+import math
 
 class Q(object):
     def  __init__(self, a, b = 1): #引数を渡す bの初期値を書いておくと省略可能
-        self.a = a  #引数は必ずself
-        self.b = b 
+        gcd = math.gcd(a, b)
+        self.a = a//gcd  #引数は必ずself
+        self.b = b//gcd
 
     def __repr__(self): #__(  )__ で表記されるメソッドは元々意味が決まっている　reprはそのオブジェクトの中身を文字列として表示する print(object)で呼び出せる
         if self.b == 1:
@@ -30,8 +32,7 @@ class Q(object):
         b = self.b * q.a
         return Q(a, b)
     
-    
-    
+
 q = Q(1, 2)
 print(q)
 
