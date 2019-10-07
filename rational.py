90420ca1491a4654ef5417f847380340997ec4c2
HASEGAWA Kimiko <21716072@jwu.ac.jp>
2019-10-07 16:14:32 +0900

    added__init__

--- rational.py
+++ rational.py
@@ -1,12 +1,19 @@
 
 class Q(object):
-    def  __init__(self, a, b): #引数を渡す
+    def  __init__(self, a, b = 1): #引数を渡す bの初期値を書いておくと省略可能
         self.a = a #引数は必ずself
         self.b = b
     
     def __repr__(self): #__(  )__ で表記されるメソッドは元々意味が決まっている　reprはそのオブジェクトの中身を文字列として表示する
-        return f"{self.a}/{self.b}"
+        if self.b == 1:
+            return str(self.a)
+        else:
+            return f"{self.a}/{self.b}"
+    
     
     
 q = Q(1, 2)
+print(q)
+
+q = Q(3)
 print(q)
