e7b9255123aefe3ae418723b55efed65f3e43dfd
HASEGAWA Kimiko <21716072@jwu.ac.jp>
2019-10-07 16:04:10 +0900

    class Q

--- rational.py
+++ rational.py
@@ -1,9 +1,12 @@
 
 class Q(object):
-    def  __init__(self): #コンストラクタ. 必ず必要
-        self.a = 0 #引数は必ずself
-        self.b = 0
+    def  __init__(self, a, b): #引数を渡す
+        self.a = a #引数は必ずself
+        self.b = b
+    
+    #def __repr__(self): #__(  )__ で表記されるメソッドは元々意味が決まっている　reprはそのオブジェクトの中身を文字列として表示する
+     #   return str(self.a / self.b)
     
     
-    
-    
+q = Q(1, 2)
+print(q)
