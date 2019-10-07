901cf60a174679a1ef66e43de68e0c2c644f4b49
HASEGAWA Kimiko <21716072@jwu.ac.jp>
2019-10-07 16:10:10 +0900

    added__repr__

--- rational.py
+++ rational.py
@@ -4,7 +4,9 @@ class Q(object):
         self.a = a #引数は必ずself
         self.b = b
     
-   
+    def __repr__(self): #__(  )__ で表記されるメソッドは元々意味が決まっている　reprはそのオブジェクトの中身を文字列として表示する
+        return f"{self.a}/{self.b}"
+    
     
 q = Q(1, 2)
 print(q)
