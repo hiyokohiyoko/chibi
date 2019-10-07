ac1e6fb95b2df9cb0dfcc608c6583ce90b50f95c
HASEGAWA Kimiko <21716072@jwu.ac.jp>
2019-10-07 16:39:29 +0900

    all operators

--- rational.py
+++ rational.py
@@ -1,11 +1,9 @@
 
 class Q(object):
     def  __init__(self, a, b = 1): #引数を渡す bの初期値を書いておくと省略可能
-        g = gcd(a, b)
-        self.a = a #引数は必ずself
-        self.b = b
+        self.a = a  #引数は必ずself
+        self.b = b 
 
-    
     def __repr__(self): #__(  )__ で表記されるメソッドは元々意味が決まっている　reprはそのオブジェクトの中身を文字列として表示する print(object)で呼び出せる
         if self.b == 1:
             return str(self.a)
@@ -17,13 +15,20 @@ class Q(object):
         b = self.b * q.b
         return Q(a, b)
 
+    def __sub__(self, q):
+        a = self.a * q.b - self.b * q.a
+        b = self.b * q.b
+        return Q(a, b)
 
-def gcd(a, b):
-    if b == 0:
-        return a
-    r = a % b
-    gcd(b, r)
+    def __mul__(self, q):
+        a = self.a * q.a
+        b = self.b * q.b
+        return Q(a, b)
 
+    def __truediv__(self, q):
+        a = self.a * q.b
+        b = self.b * q.a
+        return Q(a, b)
     
     
     
@@ -39,4 +44,10 @@ print(q)
 q1 = Q(1, 2)
 q2 = Q(1, 3)
 # print(q1.add(q2)) # = 5/6
-print(q1 + q2) #糖衣構文
+print(q1 + q2) #糖衣構文 5/6
+print(q1 - q2) # 1/6
+print(q1 * q2) # 1/6
+print(q1 / q2) # 3/2
+
+
+
