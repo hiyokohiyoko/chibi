7f685617f4c6bfbe1aaf2d9c019b39ee8c9e0a7a
HASEGAWA Kimiko <21716072@jwu.ac.jp>
2019-10-14 15:49:41 +0900

    Add

--- exp.py
+++ exp.py
@@ -9,7 +9,21 @@ class Val(object):
     def __repr__(self):
         return f'Val({self.value})'
 
-        
+class Add(object):
+    __slots__ = ['left', 'right']
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+    
+    def eval(self):
+        return self.left + self.right
+
+    
+
 e = Val(1)
 assert e.eval() == 1
 print(e.eval())
+
+v = Add(1, 2)
+assert v.eval() == 3
+print(v.eval())
