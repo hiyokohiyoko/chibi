16a06e9a4e87d25fe5a9055c7e52009752ba0af7
HASEGAWA Kimiko <21716072@jwu.ac.jp>
2019-11-25 16:51:07 +0900

    calc under construction

--- icalc.py
+++ icalc.py
@@ -13,6 +13,30 @@ print(repr(t))
 t = parser('@2') #Syntax Errorになってほしい
 print(repr(t))
 
+#以下、run()を使う場合に必要になるExpr(式)クラスの定義をする
+
+class Expr(object):
+    @classmethod
+    def new(cls, v):
+        if isinstance(v, Expr):
+            return v
+        return Val(v)
+
+class Val(Expr):
+    __slot__ = ['value']
+
+    def __init__(self, value):
+        self.value = value
+
+    def __repr__(self):
+        return f'Val({self.value})'
+
+    def eval(self, env:dict):
+        return self.value
+
+e = Val(0)
+assert e.eval({}) == 0
+
 #以下、生成されたパーサが構文解析をした結果出力される解析構文木について、実際に計算して結果を表示する関数を作る
 
 def calc(t):  #tは構文木
@@ -44,13 +68,12 @@ def main():  #Main関数を作りたい
 
 def run(s: str):
     tree = parser(s) #構文木を生成
-    e = conv(tree) #構文木を式にする
-    print(e)
-    print(e.eval())
+    ee = conv(tree) #構文木を式にする
+    print(ee)
+    print(ee.eval())
 
 def conv(tree):
-    return Val(0)
-
+    pass
 
 if __name__ == '__main__':  # >>>python icalc.py と打ち込んだら対話型のプログラムが開始されるようにする
     main()
