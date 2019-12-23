373e5877a424c4db206102cd92456df446f35d6c
hiyokohiyoko <43847589+hiyokohiyoko@users.noreply.github.com>
2019-12-23 16:42:07 +0900

    Update tkinter.py

--- tkinter.py
+++ tkinter.py
@@ -1,1 +1,15 @@
+# GUI を作りたい
+import sys
+from tkinter import Tk, ttk
+
+root = Tk()
+btnframe = ttk.frame(root)
+def button1_clicked():
+    print("大吉")
+    root.quit()
+
+btn = ttk.Button(btnframe, text='占い', command=button1_clicked())
+
+btnframe.grid()
+btn.grid()
 
