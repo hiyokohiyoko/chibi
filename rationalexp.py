0c0a2b3db2d4bd867e0d245a1394158731618a85
hiyokohiyoko <m1716072hk@ug.jwu.ac.jp>
2019-12-25 18:32:05 +0900

    AssertionError

--- rationalexp.py
+++ rationalexp.py
@@ -1,132 +1,89 @@
-# 有理数版の計算機を作る
-import math
+# 有理数としての演算を可能にする
 
 class Expr(object): # 上位クラス
     def eval(self): pass # 下位クラスに共通のメソッド　定義の内容は下位クラスごとに違うのでここでは定義しない
 
 def expr(e): #Expr(式)クラスであるかどうか判定する関数
     if not isinstance(e, Expr):
-        e = Qval(e)
+        e = Val(e)
     return e
 
 class Binary(Expr):
-    __slots__ = ['left', 'right']
-    def __init__(self, left, right):
-        self.left = left
-        self.right = right
+    __slots__ = ['a', 'b']
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
     
     def eval(self): pass # 定義の内容は下位クラスごとに違うのでここでは定義しない
 
     def __repr__(self):
         classname = self.__class__.__name__
-        return f'{classname}({self.left}, {self.right})'
+        return f'{classname}({self.a}, {self.b})'
 
-class Qval(Expr): #Exprから継承されたクラス
-    __slots__ = ['a', 'b']
-    def __init__(self, a, b = 1):
-        gcd = math.gcd(a, b)
-        self.a = a//gcd
-        self.b = b//gcd
+class Val(Expr): #Exprから継承されたクラス
+    __slots__ = ['value']
+    def __init__(self, v = 0):
+        self.value = v
+
+    def eval(self):
+        return self.value
 
     def __repr__(self):
-        if self.b == 1:
-            return str(self.a)
-        else:
-            return f'{self.a}/{self.b}'
-
-    def eval(self):
-        return self.a/self.b
-
+        return f'Val({self.value})'
 
 class Add(Binary):
-    __slots__ = ['left', 'right']
-    def __init__(self, left, right):
-        self.left = expr(left)
-        self.right = expr(right) #この2つは式として渡される
+    __slots__ = ['a', 'b']
+    def __init__(self, a, b):
+        self.a = expr(a)
+        self.b = expr(b) #この2つは式として渡される
     
     def eval(self):
-        return self.left.eval() + self.right.eval() # 元々式の値として渡し、ここで評価するようにする eQval()は中身の該当クラスのメソッド
+        aa = self.a.eval().a.eval() * self.b.eval().b.eval() + self.a.eval().b.eval() * self.b.eval().a.eval()
+        bb = self.a.eval().b.eval() * self.b.eval().b.eval()
+        return Div(aa, bb) # 元々式の値として渡し、ここで評価するようにする
 
     # reprは上位クラスBinaryで定義済
 
 class Mul(Binary):
-    __slots__ = ['left', 'right']
-    def __init__(self, left, right):
-        self.left = expr(left)
-        self.right = expr(right)
+    __slots__ = ['a', 'b']
+    def __init__(self, a, b):
+        self.a = expr(a)
+        self.b = expr(b)
     
     def eval(self):
-        return self.left.eval() * self.right.eval()
+        aa = self.a.eval().a.eval() * self.b.eval().a.eval()
+        bb = self.a.eval().b.eval() * self.b.eval().b.eval()
+        return Div(aa, bb)
 
 class Sub(Binary):
-    __slots__ = ['left', 'right']
-    def __init__(self, left, right):
-        self.left = expr(left)
-        self.right = expr(right)
+    __slots__ = ['a', 'b']
+    def __init__(self, a, b):
+        self.a = expr(a)
+        self.b = expr(b)
         
     def eval(self):
-        return self.left.eval() - self.right.eval()
+        aa = self.a.eval().a.eval() * self.b.eval().b.eval() - self.a.eval().b.eval() * self.b.eval().a.eval()
+        bb = self.a.eval().b.eval() * self.b.eval().b.eval()
+        return Div(aa, bb)
 
 class Div(Binary):
-    __slots__ = ['left', 'right']
-    def __init__(self, left, right):
-        self.left = expr(left)
-        self.right = expr(right)
+    __slots__ = ['a', 'b']
+    def __init__(self, a, b = 1):
+        self.a = expr(a)
+        self.b = expr(b)
             
     def eval(self):
-        return self.left.eval() // self.right.eval()
+        if self.a.eval().b.eval() == 1 and self.b.eval().b.eval() == 1:
+           return
+        else:
+            aa = self.a.eval().a.eval() * self.b.eval().b.eval()
+            bb = self.a.eval().b.eval() * self.b.eval().a.eval()
+            return Div(aa, bb)
 
 
+#有理数表示 1/2 + 2/3
+e = Add(Div(1, 2), Div(2, 3))
+assert e == Div(6, 7)
+print(e)
 
-    
-#val
-e = Qval(1)
-assert e.eval() == 1
-print(e.eval())
-assert isinstance(e, Expr) # 値の種類を判定 True
-assert isinstance(e, Qval) # True
-# assert isinstance(e, int) # false
-
-e = Qval(1, 2)
-assert e.eval() == 0.5
-print(e.eval())
-
-#Add
-v = Add(Qval(1), Qval(2))
-assert v.eval() == 3
-print(v.eval())
-# e = Add(1, Add(2, 3)) # これだとうまくいかない　self.rightに式が入ってしまうため
-e = Add(Qval(1), Add(Qval(2), Qval(3)))
-print(e.eval())
-
-#Mul
-e = Mul(Qval(1), Qval(2))
-assert e.eval() == 2
-print(e.eval())
-
-#Sub
-e = Sub(Qval(1), Qval(2))
-assert e.eval() == -1
-print(e.eval())
-
-#Div
-e = Div(Qval(7), Qval(2))
-assert e.eval() == 3
-print(e.eval())
-
-#class継承
-assert isinstance(Qval(1), Expr)
-assert isinstance(Div(Qval(7), Qval(2)), Expr)
-
-#シンプルに
-e = Mul(Add(1, 2), 3)
-assert e.eval() == 9
-print(e.eval())
-
-#分数表示
-e = Mul(Add(Qval(1, 2), Qval(2, 3)), Qval(6, 7))
-print(e.eval())
-
-# 1/2 + 2/3
-e = Add(Q(1, 2), Q(2, 3))
 
