074cf6d6fa8d49bcc93fa90e1c78241ab77e8755
hiyokohiyoko <m1716072hk@ug.jwu.ac.jp>
2020-01-06 01:09:47 +0900

    fib error

--- chibi.redo.py
+++ chibi.redo.py
@@ -3,40 +3,29 @@ peg = pegpy.grammar('chibi.tpeg')
 parser = pegpy.generate(peg)
 
 class Expr(object): # 上位クラス
-    def eval(self): pass # 下位クラスに共通のメソッド　定義の内容は下位クラスごとに違うのでここでは定義しない
-    
-def expr(e): #Expr(式)クラスであるかどうか判定する関数
-    if not isinstance(e, Expr):
-        e = Val(e)
-    return e
+    # def eval(self): pass # 下位クラスに共通のメソッド　定義の内容は下位クラスごとに違うのでここでは定義しない
+    @classmethod
+    def new(cls, v):
+        if isinstance(v, Expr):
+            return v
+        return Val(v)
 
-class Var(Expr):
-    __slots__ = ['name']
-    def __init__(self, s: str):
-        self.name = s
+class Val(Expr): #Exprから継承されたクラス
+    __slots__ = ['value']
+    def __init__(self, v):
+        self.value = v
+
     def __repr__(self):
-        return self.name
+        return f'Val({self.value})'
+        
     def eval(self, env: dict):
-        if self.name in env:
-            return env[self.name]
-        else:
-            raise NameError(self.name)
-
-class Assign(Expr):
-    __slots__ = ['name', 'expr']
-    def __init__(self, s: str, ex: Expr):
-        self.name = s
-        self.expr = expr(ex)
-
-    def eval(self, env: dict):
-        env[self.name] = self.expr.eval(env)
-        return env[self.name]
+        return self.value
 
 class Binary(Expr):
     __slots__ = ['left', 'right']
     def __init__(self, left, right):
-        self.left = left
-        self.right = right
+        self.left = Expr.new(left)
+        self.right = Expr.new(right) #ここで式として渡しておく
     
     def eval(self): pass # 定義の内容は下位クラスごとに違うのでここでは定義しない
 
@@ -44,117 +33,71 @@ class Binary(Expr):
         classname = self.__class__.__name__
         return f'{classname}({self.left}, {self.right})'
 
-class Val(Expr): #Exprから継承されたクラス
-    __slots__ = ['value']
-    def __init__(self, v = 0):
-        self.value = v
-
-    def eval(self, env: dict):
-        return self.value
-
-    def __repr__(self):
-        return f'Val({self.value})'
-
-class Add(Binary):
+class Add(Binary):# repr, initは上位クラスBinaryで定義済
     __slots__ = ['left', 'right']
-    def __init__(self, left, right):
-        self.left = expr(left)
-        self.right = expr(right) #この2つは式として渡される
-    
     def eval(self, env: dict):
         return self.left.eval(env) + self.right.eval(env) # 元々式の値として渡し、ここで評価するようにする
-
-    # reprは上位クラスBinaryで定義済
-
-class Mul(Binary):
-    __slots__ = ['left', 'right']
-    def __init__(self, left, right):
-        self.left = expr(left)
-        self.right = expr(right)
-    
-    def eval(self, env: dict):
-        return self.left.eval(env) * self.right.eval(env)
-
 class Sub(Binary):
-    __slots__ = ['left', 'right']
-    def __init__(self, left, right):
-        self.left = expr(left)
-        self.right = expr(right)
-        
+    __slots__ = ['left', 'right'] 
     def eval(self, env: dict):
         return self.left.eval(env) - self.right.eval(env)
-
-class Div(Binary):
+class Mul(Binary):
     __slots__ = ['left', 'right']
-    def __init__(self, left, right):
-        self.left = expr(left)
-        self.right = expr(right)
-            
+    def eval(self, env: dict):
+        return self.left.eval(env) * self.right.eval(env)
+class Div(Binary):
+    __slots__ = ['left', 'right']    
     def eval(self, env: dict):
         return self.left.eval(env) / self.right.eval(env)
-
 class Mod(Binary):
-    __slots__ = ['left', 'right']
-    def __init__(self, left, right):
-        self.left = expr(left)
-        self.right = expr(right)
-            
+    __slots__ = ['left', 'right']    
     def eval(self, env: dict):
         return self.left.eval(env) % self.right.eval(env)
-
 class Eq(Binary):
     __slots__ = ['left', 'right']
-    def __init__(self, left, right):
-        self.left = expr(left)
-        self.right = expr(right)
-    
     def eval(self, env: dict):
         return 1 if self.left.eval(env) == self.right.eval(env) else 0
-
 class Ne(Binary):
     __slots__ = ['left', 'right']
-    def __init__(self, left, right):
-        self.left = expr(left)
-        self.right = expr(right)
-    
     def eval(self, env: dict):
         return 1 if self.left.eval(env) != self.right.eval(env) else 0
-
 class Lt(Binary):
     __slots__ = ['left', 'right']
-    def __init__(self, left, right):
-        self.left = expr(left)
-        self.right = expr(right)
-    
     def eval(self, env: dict):
         return 1 if self.left.eval(env) < self.right.eval(env) else 0
-
 class Lte(Binary):
     __slots__ = ['left', 'right']
-    def __init__(self, left, right):
-        self.left = expr(left)
-        self.right = expr(right)
-    
     def eval(self, env: dict):
         return 1 if self.left.eval(env) <= self.right.eval(env) else 0
-
 class Gt(Binary):
     __slots__ = ['left', 'right']
-    def __init__(self, left, right):
-        self.left = expr(left)
-        self.right = expr(right)
-    
     def eval(self, env: dict):
         return 1 if self.left.eval(env) > self.right.eval(env) else 0
-
 class Gte(Binary):
     __slots__ = ['left', 'right']
-    def __init__(self, left, right):
-        self.left = expr(left)
-        self.right = expr(right)
-    
     def eval(self, env: dict):
         return 1 if self.left.eval(env) >= self.right.eval(env) else 0
+
+class Var(Expr):
+    __slots__ = ['name']
+    def __init__(self, s):
+        self.name = s
+    def __repr__(self):
+        return self.name
+    def eval(self, env: dict):
+        if self.name in env:
+            return env[self.name]
+        raise NameError(self.name)
+
+class Assign(Expr):
+    __slots__ = ['name', 'expr']
+    def __init__(self, s, ex):
+        self.name = s
+        self.expr = Expr.new(ex)
+
+    def eval(self, env: dict):
+        env[self.name] = self.expr.eval(env)
+        return env[self.name]
 
 class Block(Expr):
     __slots__ = ['exprs']
@@ -164,19 +107,6 @@ class Block(Expr):
     def eval(self, env):
         for e in self.exprs:  # 逐次処理の本質的な部分はここ
             e.eval(env)
-
-class If(Expr):
-    __slots__ = ['cond', 'then', 'else_']
-    def __init__ (self, cond, then, else_):
-        self.cond = cond
-        self.then = then
-        self.else_ = else_
-    
-    def eval(self, env):
-        if self.cond.eval(env) != 0:
-            return self.then.eval(env)
-        else:
-            return self.else_.eval(env)
 
 class While(Expr):
     __slots__ = ['cond', 'body_']
@@ -191,82 +121,95 @@ class While(Expr):
             except ValueError:
                 break
 
+class If(Expr):
+    __slots__ = ['cond', 'then', 'else_']
+    def __init__ (self, cond, then, else_):
+        self.cond = cond
+        self.then = then
+        self.else_ = else_
+    
+    def eval(self, env):
+        if self.cond.eval(env) != 0:
+            return self.then.eval(env)
+        else:
+            return self.else_.eval(env)
+
 class Break(Expr):
     def eval(self, env):
         raise ValueError
 
 class Lambda(Expr):
     __slots__ = ['name', 'body']
-    def __init__ (self, name: str, body: Expr):
+    def __init__(self, name, body):
         self.name = name
         self.body = body
-    def __repr__ (self):
+    def __repr__(self):
         return f'λ{self.name} . {str(self.body)}'
     def eval(self, env):
-        return self.body
+        return self
+
+def copy(env):
+    newenv = {}
+    for x in env.keys():
+        newenv[x] = env[x]
+    return newenv
 
 class FuncApp(Expr):
     __slots__ = ['func', 'param'] # funcはλ式
-    def __init__ (self, func: Lambda, param: Expr):
+    def __init__(self, func: Lambda, param):
         self.func = func
-        self.param = expr(param)
-    def __repr__ (self):
+        self.param = Expr.new(param)
+    def __repr__(self):
         return f'({repr(self.func)}) ({repr(self.param)})'
     def eval (self, env):
         f = self.func.eval(env)
         p = self.param.eval(env)
+        name = f.name
         env = copy(env)
-        env[f.name] = p # 正格評価
+        env[name] = p # 正格評価
         return f.body.eval(env)
 
-def copy(env):
-    localenv = {}
-    for x in env.keys():
-        localenv[x] = env[x]
-    return localenv
-
-def conv(t):
-    #print(repr(t)) # 評価される前の構造木を表示
-    if t.tag == 'Block':
-        return conv(t[0])
-    if t.tag == 'Int':
-        return Val(int(str(t)))
-    if t.tag == 'Add':
-        return Add(conv(t[0]), conv(t[1]))
-    if t.tag == 'Sub':
-        return Sub(conv(t[0]), conv(t[1]))
-    if t.tag == 'Mul':
-        return Mul(conv(t[0]), conv(t[1]))
-    if t.tag == 'Div':
-        return Div(conv(t[0]), conv(t[1]))
-    if t.tag == 'Mod':
-        return Mod(conv(t[0]), conv(t[1]))
-    if t.tag == 'Eq':
-        return Eq(conv(t[0]), conv(t[1]))
-    if t.tag == 'Ne':
-        return Ne(conv(t[0]), conv(t[1]))
-    if t.tag == 'Lt':
-        return Lt(conv(t[0]), conv(t[1]))
-    if t.tag == 'Lte':
-        return Lte(conv(t[0]), conv(t[1]))
-    if t.tag == 'Gt':
-        return Gt(conv(t[0]), conv(t[1]))
-    if t.tag == 'Gte':
-        return Gte(conv(t[0]), conv(t[1]))
-    if t.tag == 'Var':
-        return Var(str(t))
-    if t.tag == 'LetDecl':
-        return Assign(str(t[0]), conv(t[1]))
-    if t.tag == 'If':
-        return If(conv(t[0]), conv(t[1]), conv(t[2]))
-    if t.tag == 'While':
-        return While(conv(t[0]), conv(t[1]))
-    if t.tag == 'Funcdecl':
-        return Assign(str(t[0]), Lambda(str(t[1]), conv(t[2])))
-    if t.tag == 'FuncApp':
-        return FuncApp(conv(t[0]), conv(t[1]))
-    return Val(str(t))
-
+def conv(tree):
+    #print(repr(tree)) # 評価される前の構造木を表示
+    if tree == 'Block':
+        return conv(tree[0])
+    if tree == 'Funcdecl':
+        return Assign(str(tree[0]), Lambda(str(tree[1]), conv(tree[2])))
+    if tree == 'FuncApp':
+        return FuncApp(conv(tree[0]), conv(tree[1]))
+    if tree == 'If':
+        return If(conv(tree[0]), conv(tree[1]), conv(tree[2]))
+    if tree == 'While':
+        return While(conv(tree[0]), conv(tree[1]))
+    if tree == 'Int' or tree == 'Val':
+        return Val(int(str(tree)))
+    if tree == 'Add':
+        return Add(conv(tree[0]), conv(tree[1]))
+    if tree == 'Sub':
+        return Sub(conv(tree[0]), conv(tree[1]))
+    if tree == 'Mul':
+        return Mul(conv(tree[0]), conv(tree[1]))
+    if tree == 'Div':
+        return Div(conv(tree[0]), conv(tree[1]))
+    if tree == 'Mod':
+        return Mod(conv(tree[0]), conv(tree[1]))
+    if tree == 'Eq':
+        return Eq(conv(tree[0]), conv(tree[1]))
+    if tree == 'Ne':
+        return Ne(conv(tree[0]), conv(tree[1]))
+    if tree == 'Lt':
+        return Lt(conv(tree[0]), conv(tree[1]))
+    if tree == 'Lte':
+        return Lte(conv(tree[0]), conv(tree[1]))
+    if tree == 'Gt':
+        return Gt(conv(tree[0]), conv(tree[1]))
+    if tree == 'Gte':
+        return Gte(conv(tree[0]), conv(tree[1]))
+    if tree == 'Var':
+        return Var(str(tree))
+    if tree == 'LetDecl':
+        return Assign(str(tree[0]), conv(tree[1]))
+    return Val(str(tree))
 
 def run(s: str, env: dict):
     tree = parser(s)
@@ -354,4 +297,4 @@ print(e.eval({}))
 env = {}
 f = Lambda('x', Lambda('y', Add(Var('x'), Var('y'))))
 e = FuncApp(FuncApp(f, Val(1)), Val(2))
-assert e.eval(env) == 3
+# assert e.eval(env) == 3
