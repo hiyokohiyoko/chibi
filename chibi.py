39ceeea1ab27718e5e7d3780468a10740bb05d5e
hiyokohiyoko <m1716072hk@ug.jwu.ac.jp>
2020-01-06 01:09:35 +0900

    correction

--- chibi.py
+++ chibi.py
@@ -14,16 +14,18 @@ class Expr(object):
         if isinstance(v, Expr):
             return v
         return Val(v)
+
 class Val(Expr):
     __slots__ = ['value']
     def __init__(self, value):
         self.value = value
+
     def __repr__(self):
         return f'Val({self.value})'
+
     def eval(self, env: dict):
         return self.value
-e = Val(0)
-assert e.eval({}) == 0
+
 class Binary(Expr):
     __slots__ = ['left', 'right']
     def __init__(self, left, right):
@@ -32,6 +34,7 @@ class Binary(Expr):
     def __repr__(self):
         classname = self.__class__.__name__
         return f'{classname}({self.left},{self.right})'
+
 class Add(Binary):
     __slots__ = ['left', 'right']
     def eval(self, env: dict):
@@ -76,6 +79,7 @@ class Gte(Binary): # left != right
     __slots__ = ['left', 'right']
     def eval(self, env: dict):   # cond ? x : y
         return 1 if self.left.eval(env) >= self.right.eval(env) else 0
+
 class Var(Expr):
     __slots__ = ['name']
     def __init__(self, name):
@@ -86,35 +90,43 @@ class Var(Expr):
         if self.name in env:
             return env[self.name]
         raise NameError(self.name)
+
 class Assign(Expr):
     __slots__ = ['name', 'e']
     def __init__(self, name, e):
         self.name = name
         self.e = Expr.new(e)
+
     def eval(self, env):
         env[self.name] = self.e.eval(env)
         return env[self.name]
+
 class Block(Expr):
     __slots__ = ['exprs']
     def __init__(self, *exprs): # 可変長個の引数
         self.exprs = exprs  # [e, e2, e3, e4, e5] リストになっている
+    
     def eval(self, env):
         for e in self.exprs:
             e.eval(env)
+
 class While(Expr):
     __slots__ = ['cond', 'body']
     def __init__(self, cond, body):
         self.cond = cond
         self.body = body
+
     def eval(self, env):
         while self.cond.eval(env) != 0:
             self.body.eval(env)
+
 class If(Expr):
     __slots__ = ['cond', 'then', 'else_']
     def __init__(self, cond, then, else_ ):
         self.cond = cond
         self.then = then
         self.else_ = else_
+    
     def eval(self, env):
         yesorno = self.cond.eval(env)
         if yesorno == 1:
@@ -130,11 +142,13 @@ class Lambda(Expr):
         return f'λ{self.name} . {str(self.body)}'
     def eval(self, env):
         return self
+
 def copy(env): #環境をコピーすることでローカルスコープを作る
     newenv = {}
     for x in env.keys():
         newenv[x] = env[x]
     return newenv
+
 class FuncApp(Expr):
     __slots__ = ['func', 'param']
     def __init__(self, func: Lambda, param):
@@ -149,6 +163,7 @@ class FuncApp(Expr):
         env = copy(env)  # 環境をコピーすることでローカルスコープを作る
         env[name] = v   # 環境から引数を渡す
         return f.body.eval(env)
+
 def conv(tree):
     if tree == 'Block':
         return conv(tree[0])
@@ -190,14 +205,17 @@ def conv(tree):
         return Assign(str(tree[0]), conv(tree[1]))
     print('@TODO', tree.tag, repr(tree))
     return Val(str(tree))
+
 def run(src: str, env: dict):
     tree = parser(src)
     if tree.isError():
         print(repr(tree))
     else:
         e = conv(tree)
-        #print('env', env)
+        print(repr(e))
+        print('env', env)
         print(e.eval(env))
+
 def main():
     try:
         env = {}
@@ -208,9 +226,29 @@ def main():
             run(s, env)
     except EOFError:
         return
+
 if __name__ == '__main__':
     main()
 
+
+# modified eval()
+e = Mul(Val(1), Val(2))
+assert e.eval({}) == 2
+
+# added comere
+e = Lt(Val(0), Val(1))
+assert e.eval({}) == 1
+e = Gt(Val(0), Val(1))
+assert e.eval({}) == 0
+
+# added Var
+e = Var('x')
+assert e.eval({'x':1}) == 1
+
+# added Assign
+env = {}
+e = Assign('x', Val(1))
+assert e.eval(env) == 1
 
 # Block
 e = Block(
@@ -218,7 +256,7 @@ e = Block(
     Assign('x', Add(Var('x'), Val(1))),
     #Var('x')
 )
-# assert(e.eval({})) == 2
+# assert e.eval({}) == 2
 
 # If
 e = Block(
@@ -226,4 +264,32 @@ e = Block(
     Assign('y', 1),
     If(Gt(Var('x'), Var('y')), Var('x'), Var('y'))
 )
-# assert(e.eval({})) == 2
+# assert e.eval({}) == 2
+
+# While
+e = Block(
+    Assign('x', Val(0)),
+    While(Lt(Var('x'), Val(10)),
+        Assign('x', Add(Var('x'), Val(1))) ),
+    Var('x')
+)
+# assert e.eval({}) == 10
+
+# Break
+e = Block(
+    Assign('x', Val(0)),
+    While(Lt(Var('x'), Val(10)),
+        If(Eq(Var('x'), Val(5)),
+            Break(),
+            Assign('x', Add(Var('x'), Val(1))))
+    ),
+    Var('x')
+)
+# assert e.eval({}) == 5
+print(e.eval({}))
+
+# Currying
+env = {}
+f = Lambda('x', Lambda('y', Add(Var('x'), Var('y'))))
+e = FuncApp(FuncApp(f, Val(1)), Val(2))
+# assert e.eval(env) == 3
